5b46b87a99903df79d39798ffbcf9de240d59727
lilhope <lilhope@163.com>
2017-05-11 16:44:16 +0800

    some scripts for easy usage

---
+++ script/additional_deps.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# install additional depts
+sudo apt install python-pip python-dev unzip python-matplotlib
+sudo pip install cython scikit-image easydict
---
+++ script/train_res_e2e.sh
@@ -0,0 +1,3 @@
+export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
+export PYTHONUNBUFFERED=1
+python train_end2end.py
